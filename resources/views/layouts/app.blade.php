c748833d113aba36d70c45755bc71440cd7f734c
Adrian Metanoia Gawa <tfadriangawa@gmail.com>
2022-10-08 00:07:26 +0700

    setup layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @notifyCss
 </head>
 <body>
     <div id="app">
@@ -79,5 +80,7 @@
             @yield('content')
         </main>
     </div>
+    <x:notify-messages />
+    @notifyJs
 </body>
 </html>
